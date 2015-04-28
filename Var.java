ffcd3b340f99f788631c4c6eec23c6de31916468
Vitor Forbrig <vitorforbrig@gmail.com>
2015-04-28 17:52:46 -0300

    Added class "Var.java"

---
+++ Var.java
@@ -0,0 +1,28 @@
+/*
+ this is the class that store all program variables
+*/
+class Var {
+	private String name;
+	private double value;
+	
+	public Var(String s, double d) { //the constructor
+		this.name = s;
+		this.value = d;
+	}
+	
+	public void setName(String p) {
+		this.name = p;
+	}
+	
+	public void setValue(double n) {
+		this.value = n;
+	}
+	
+	public String getNome() {
+		return this.name;
+	}
+	
+	public double getValor() {
+		return this.value;
+	}
+}
