1c2fc1873da147ca2332a05383ee6b20ef5bb179
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2015-05-14 04:32:54 -0300

    Added comment support; check TO-DO list on Interpreter.java file

--- Fonte/Interpreter.java
+++ Fonte/Interpreter.java
@@ -3,6 +3,11 @@ class Interpreter {
 	private int top; //contador de números de variáveis
 	private Arit arit;
 	
+	/* TO-DO: (mero detalhe)
+	 * substituir o método de armazenamento
+	 * das variáveis por algo mais eficiente, 
+	 * como o TreeSet do Java.
+	*/
 	public Interpreter( ) { //init the interpretar already whit "variable"
 		this.variable = new Var[1000];
 		this.arit = new Arit();
@@ -15,8 +20,9 @@ class Interpreter {
 			if (lines[i] != null) {
 				//a string no indice será quebrada em várias substrings para ser interpretada
 				String treated[];
-				treated = lines[i].split(" "); //linha tratada a ser interpretada
-				
+				treated = lines[i].split("~"); // Esta linha dispensa comentarios
+				// Trocadilho nao-intencional :v
+				treated = treated[0].split(" "); //linha tratada a ser interpretada
 				//interpreta a linha
 				int k;
 				switch (treated[0].toLowerCase()) {
@@ -34,6 +40,21 @@ class Interpreter {
 						double resultado = arit.avaliaExpressao(treated, 1, variable, top);
 						System.out.println(String.valueOf(resultado));
 						break;
+						
+					/* TO-DO:
+					 * implementar comparação entre variáveis
+					 * lembrar da tolerância de precisão, EPS = 1e-9
+					 * menor que: 		"LT"
+					 * menor ou igual: 	"LEQ"
+					 * igual: 			"EQ"
+					 * maior ou igual: 	"GEQ"
+					 * maior: 			"GT"
+					 * diferente:		"NEQ" (Not EQual)
+					 * 
+					 * implementar "WHILE" (delimitado por DONE)
+					 * implementar "IF" (delimitado por ENDIF)
+					 * se possível, com "ELSE" e/ou "ELIF"
+					*/
 					default:
 						System.err.println("Syntax Error: " + treated[0]);
 						break;
